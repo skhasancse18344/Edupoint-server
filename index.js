37664fbaf6d74befe587d9b2797a81ed9b5d525c
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-10-26 01:27:21 +0600

    Checkout

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const courses = require("./data/Course.json");
+const checkout = require("./data/Course.json");
 
 app.listen(port, () => {
   console.log("Server running on", port);
@@ -13,6 +14,12 @@ app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const selectedCourse = courses.find((course) => course.id == id);
   res.send(selectedCourse);
+});
+app.get("/checkout/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectCheckout = checkout.find((course) => course.id == id);
+  res.send(selectCheckout);
 });
 app.get("/courses", (req, res) => {
   res.send(courses);
