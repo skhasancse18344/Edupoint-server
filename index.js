ce8691add383eba519d1c7274492cbd16e1b89d4
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-10-25 19:58:19 +0600

    Course Catagory

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const cors = require("cors");
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+const courses = require("./data/Course.json");
+
 app.listen(port, () => {
   console.log("Server running on", port);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
